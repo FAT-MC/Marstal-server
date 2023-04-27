e06a2cc55e2555031529bc8da698d0b518002657
Marko Ma <markomadev@gmail.com>
2023-04-27 22:43:58 +0800

    implemented TTS service using GCP TTS SDK

---
+++ src/service/ttsService.js
@@ -0,0 +1,28 @@
+const textToSpeech = require('@google-cloud/text-to-speech');
+
+// Creates a client
+const client = new textToSpeech.TextToSpeechClient();
+
+const getSynthesizedAudio = async (text) => {
+  // Construct the request
+  const request = {
+    input: { text: text },
+    // Select the language and SSML voice gender (optional)
+    voice: { languageCode: 'en-US', name: 'en-US-Neural2-C' },
+    // select the type of audio encoding
+    audioConfig: {
+      audioEncoding: 'MP3',
+      pitch: 0,
+      speakingRate: 1
+    },
+  };
+
+  // Performs the text-to-speech request
+  const [response] = await client.synthesizeSpeech(request);
+  console.log(response.audioContent);
+  return response.audioContent;
+}
+
+module.exports = {
+  getSynthesizedAudio
+}
